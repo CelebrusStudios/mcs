8f457b40a1779e4b44660e8d808ee65c32955625
kartalguner <kartalguner@celebrusstudios.com>
2015-12-24 16:14:12 -0500

    Update EyeControl.cs
    Made script more editor friendly.

--- EyeControl.cs
+++ EyeControl.cs
@@ -8,32 +8,36 @@ public class EyeControl : MonoBehaviour
     System.Random r;
 
     // Blink time
-    int minBlinkTime = 100;
-    int maxBlinkTime = 400;
+    [Tooltip("The minimum length of time a blink will take.")]
+    public int MinBlinkTime = 100;
+    [Tooltip("The maximum length of time a blink will take.")]
+    public int MaxBlinkTime = 400;
 
     // Time between blinks
-    int minBlinkRate = 2000;
-    int maxBlinkRate = 10000;
+    [Tooltip("The minimum length of time between blinks.")]
+    public int MinBlinkRate = 2000;
+    [Tooltip("The maximum length of time between blinks.")]
+    public int MaxBlinkRate = 10000;
 
     void Start ()
     {
         m = GetComponent<M3DCharacterManager>();
         r = new System.Random();
         
-        Invoke("BlinkTimer", r.Next(minBlinkRate, maxBlinkRate) / 1000);
+        Invoke("BlinkTimer", r.Next(MinBlinkRate, MaxBlinkRate) / 1000);
     }
 
     void BlinkTimer()
     {
         StartCoroutine("Blink");
-        Invoke("BlinkTimer", r.Next(minBlinkRate, maxBlinkRate) / 1000);
+        Invoke("BlinkTimer", r.Next(MinBlinkRate, MaxBlinkRate) / 1000);
     }
 
     IEnumerator Blink ()
     {
         m.SetBlendshapeValue("PHMEyesClosedR", 100);
         m.SetBlendshapeValue("PHMEyesClosedL", 100);
-        yield return new WaitForSeconds(r.Next(minBlinkTime, maxBlinkTime) / 1000f);
+        yield return new WaitForSeconds(r.Next(MinBlinkTime, MaxBlinkTime) / 1000f);
         m.SetBlendshapeValue("PHMEyesClosedR", 0);
         m.SetBlendshapeValue("PHMEyesClosedL", 0);
     }
